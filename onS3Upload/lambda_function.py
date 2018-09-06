e26f0dfa064a8fedf4bc103c9ddd64016fbf9884
Rob Rose <RobRoseKnows@gmail.com>
2018-09-06 17:08:12 -0400

    Added beginning of blur faces
    - Added code from SAM examples.
    - Modified the template a tad bit.

---
+++ onS3Upload/lambda_function.py
@@ -0,0 +1,45 @@
+# Adapted from: https://github.com/awslabs/serverless-application-model/blob/master/examples/apps/rekognition-python/lambda_function.py
+
+import boto3
+from decimal import Decimal
+import json
+import urllib
+
+print('Loading function')
+
+rekognition = boto3.client('rekognition')
+
+
+# --------------- Helper Functions to call Rekognition APIs ------------------
+
+
+def detect_faces(bucket, key):
+    response = rekognition.detect_faces(Image={"S3Object": {"Bucket": bucket, "Name": key}})
+    return response
+
+
+# --------------- Main handler ------------------
+
+
+def lambda_handler(event, context):
+    '''Demonstrates S3 trigger that uses
+    Rekognition APIs to detect faces, labels and index faces in S3 Object.
+    '''
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
+    try:
+        # Calls rekognition DetectFaces API to detect faces in S3 object
+        response = detect_faces(bucket, key)
+
+        # Print response to console.
+        print(response)
+
+        return response
+    except Exception as e:
+        print(e)
+        print("Error processing object {} from bucket {}. ".format(key, bucket) +
+              "Make sure your object and bucket exist and your bucket is in the same region as this function.")
+        raise e
